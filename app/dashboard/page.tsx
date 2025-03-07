1ce59d8f4e3b236a3a70d68513c27f828b220f3f
febx1 <60842703+febx1@users.noreply.github.com>
2025-03-07 21:00:53 +0530

    Create Post
    - Middleware
    - Inputs
    - Server actions
    - Image optimization
    - Blog Card
    - Submit button and Useformstatus(use client)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,39 @@
+import { BlogPostCard } from "@/components/general/BlogpostCard";
+import { buttonVariants } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+
+async function getData(id: string) {
+  const data = await prisma.blogPost.findMany({
+    where: {
+      authorId: id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data;
+}
+
 export default async function DasbhoardRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user.id);
+
   return (
     <div className="">
-      <h1>Hello from dashboard</h1>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Article</h2>
+        <Link className={buttonVariants()} href="/dashboard/create">
+          Create Post
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
+          <BlogPostCard key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 }
